044c7193d4d412e24ffce0059a5e0f6b3975e772
rhobro <robozmat@gmail.com>
2021-07-04 18:01:03 +0100

    Update split.go

--- pkg/mp4/split.go
+++ pkg/mp4/split.go
@@ -7,6 +7,6 @@ import (
 
 func Split(path string, chunkKBS int) error {
 	// TODO use go bindings for mp4box
-	cmd := exec.Command("/Applications/GPAC.app/Contents/MacOS/mp4box", "-splits", strconv.Itoa(chunkKBS), path)
+	cmd := exec.Command("mp4box", "-splits", strconv.Itoa(chunkKBS), path)
 	return cmd.Run()
 }
